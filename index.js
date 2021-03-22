676bdae636a6b6061892f9a06f25b7be3f863681
Samme Janderson <samme.san@gmail.com>
2021-03-22 11:33:50 -0300

    created enemies

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ canvas.height = innerHeight
 
 
 
-const x = innerWidth / 2
-const y = innerHeight / 2
 
 class Player {
     constructor(x, y, radius, color) {
@@ -55,26 +53,78 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        context.fillStyle = this.color
+        context.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 
 
 
 
-
-const player = new Player(x, y, 30, 'green')
-
+const player = new Player(innerWidth / 2, innerHeight / 2, 30, 'green')
 
 
 const projectiles = [];
+const enemies = [];
+
+
+function spawEnemies() {
+    setInterval(() => {
+        const radius =Math.random() * (25 -10) +10;
+
+        let x
+        let y
+
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height;
+            //  y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }else{
+            x = Math.random() * canvas.width + radius
+             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+
+        }
 
 
 
+        const color = 'orange'
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000)
+}
 
 function animate() {
     requestAnimationFrame(animate)
-    context.clearRect(0,0,canvas.width,canvas.height)
+    context.clearRect(0, 0, canvas.width, canvas.height)
     player.draw();
     projectiles.forEach(projectile => {
         projectile.update();
+    })
+
+    enemies.forEach(enemy => {
+        enemy.update();
     })
 }
 
@@ -97,3 +147,4 @@ window.addEventListener('click', (event) => {
 
 
 animate();
+spawEnemies();
